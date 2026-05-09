5328d844ebd09aabd3f8b23f594caaeef5c9193d
achmadfauzanhusain <achmadfauzan1340@gmail.com>
2026-05-10 01:37:09 +0700

    placeMarketOrder & placeLimitOrder

--- app/spot/controller.js
+++ app/spot/controller.js
@@ -18,9 +18,31 @@ module.exports = {
     },
     placeMarketOrder: async(req, res) => {
         try {
+            const { quantity } = req.body
+
             const client = new Spot(binanceApiKey, binanceSecretKey)
             const response = await client.newOrder('BTCUSDT', 'BUY', 'MARKET', {
-                quantity: 1,
+                quantity: quantity,
+            })
+            res.status(200).json({
+                data: response
+            })
+        } catch (error) {
+            res.status(500).json({ 
+                message: "Internal Server Error",
+                error: error.response?.data || error.message  // tampilkan error dari Binance
+            });
+        }
+    },
+    placeLimitOrder: async(req, res) => {
+        try {
+            const { quantity, price } = req.body
+
+            const client = new Spot(binanceApiKey, binanceSecretKey)
+            const response = await client.newOrder('BTCUSDT', 'BUY', 'LIMIT', {
+                price: price,
+                quantity: quantity,
+                timeInForce: 'GTC'
             })
             res.status(200).json({
                 data: response
--- app/spot/router.js
+++ app/spot/router.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getAccount, placeMarketOrder } = require("./controller")
+const { getAccount, placeMarketOrder, placeLimitOrder } = require("./controller")
 
 router.get("/account", getAccount)
-router.get("/placeOrder", placeMarketOrder)
+router.post("/placeOrder", placeMarketOrder)
+router.post("/limitOrder", placeLimitOrder)
 
 module.exports = router
