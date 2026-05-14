8a0f1d6e3d14bb1d7a28790b0341a943512fdfa7
achmadfauzanhusain <achmadfauzan1340@gmail.com>
2026-05-15 04:01:30 +0700

    feat(placeOcooOrder): take profit & stop loss func

--- app/spot/controller.js
+++ app/spot/controller.js
@@ -47,5 +47,31 @@ module.exports = {
                 error: error.response?.data || error.message
             });
         }
+    },
+    placeOcooOrder: async(req, res) => {
+        try {
+            const { quantity, abovePrice, belowStopPrice, belowPrice } = req.body
+
+            // takeprofit di price
+            // order jual dipasang di stopLimitPrice
+            // eksekusi di stopPrice
+
+            const client = new Spot(binanceApiKey, binanceSecretKey)
+            const response = await client.newOCOOrder("BTCUSDT", 'SELL', quantity, "LIMIT_MAKER", "STOP_LOSS_LIMIT", {
+                abovePrice,
+                belowStopPrice,
+                belowPrice,
+                belowTimeInForce: "GTC"
+            })
+
+            res.status(200).json({
+                data: response.data
+            }) 
+        } catch (error) {
+            res.status(500).json({ 
+                message: "Internal Server Error",
+                error: error.response?.data || error.message
+            });
+        }
     }
 }
--- app/spot/router.js
+++ app/spot/router.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { placeMarketOrder, placeLimitOrder } = require("./controller")
+const { placeMarketOrder, placeLimitOrder, placeOcooOrder } = require("./controller")
 
 router.post("/placeOrder", placeMarketOrder)
 router.post("/limitOrder", placeLimitOrder)
+router.post("/ocooOrder", placeOcooOrder)
 
 module.exports = router
